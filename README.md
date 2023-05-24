d5935b836650f4a961b80dc9d4d084ded55ba3b5
pratyush1718 <pratyush.chand@outlook.com>
2023-05-24 13:57:37 -0700

    updates

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 8 - Starter
+
+- Question 1: I would fit the automated tests within a github action that runs whenever code is pushed. Doing this, instead of waiting till end of developement or doing tests manually, we are able to establish a robust testing process that ensures consistent and reliable code quality *throughout* our Recipe project development pipeline (instead of just at the end or doing tests that take extra time).
+- Question 2: No
+- Question 3: Units tests wouldn't be best for the message feature. I wouldn't use it because the message feature performs multiple functions: read and write. As such, with a unit test, we wouldn't be able to test how these individual components interact with each other on an application/feature level. 
+- Question 4: I would use unit tests here since it tests one very specific function: capping the length of a message to 80 chars.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('isPhoneNumber returns true for valid phone numbers - Test 1', () => {
+  expect(functions.isPhoneNumber('(123) 456-7890')).toBe(true);
+});
+
+test('isPhoneNumber returns true for valid phone numbers - Test 2', () => {
+  expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+
+test('isPhoneNumber returns false for invalid phone numbers - Test 1', () => {
+  expect(functions.isPhoneNumber('1234567890')).toBe(false);
+});
+
+test('isPhoneNumber returns false for invalid phone numbers - Test 2', () => {
+  expect(functions.isPhoneNumber('(123) 456-089')).toBe(false);
+});
+
+// Unit tests for isEmail
+test('isEmail returns true for valid emails - Test 1', () => {
+  expect(functions.isEmail('hiii@heyyyy.com')).toBe(true);
+});
+
+test('isEmail returns true for valid emails - Test 2', () => {
+  expect(functions.isEmail('pchand@ucsd.edu')).toBe(true);
+});
+
+test('isEmail returns false for invalid emails - Test 1', () => {
+  expect(functions.isEmail('hiii@heyyyy')).toBe(false);
+});
+
+test('isEmail returns false for invalid emails - Test 2', () => {
+  expect(functions.isEmail('pchand.ucsd')).toBe(false);
+});
+
+// Unit tests for isStrongPassword
+test('isStrongPassword returns true for strong passwords - Test 1', () => {
+  expect(functions.isStrongPassword('Abcd1234')).toBe(true);
+});
+
+test('isStrongPassword returns true for strong passwords - Test 2', () => {
+  expect(functions.isStrongPassword('Passes_123')).toBe(true);
+});
+
+test('isStrongPassword returns false for weak passwords - Test 1', () => {
+  expect(functions.isStrongPassword('ab')).toBe(false);
+});
+
+test('isStrongPassword returns false for weak passwords - Test 2', () => {
+  expect(functions.isStrongPassword('1234')).toBe(false);
+});
+
+// Unit tests for isDate
+test('isDate returns true for valid dates - Test 1', () => {
+  expect(functions.isDate('01/01/2022')).toBe(true);
+});
+
+test('isDate returns true for valid dates - Test 2', () => {
+  expect(functions.isDate('01/18/2023')).toBe(true);
+});
+
+test('isDate returns false for invalid dates - Test 1', () => {
+  expect(functions.isDate('12/01/20222')).toBe(false);
+});
+
+test('isDate returns false for invalid dates - Test 2', () => {
+  expect(functions.isDate('2022/01/01')).toBe(false);
+});
+
+// Unit tests for isHexColor
+test('isHexColor returns true for valid hex colors - Test 1', () => {
+  expect(functions.isHexColor('#FF0000')).toBe(true);
+});
+
+test('isHexColor returns true for valid hex colors - Test 2', () => {
+  expect(functions.isHexColor('#ABC123')).toBe(true);
+});
+
+test('isHexColor returns false for invalid hex colors - Test 1', () => {
+  expect(functions.isHexColor('nothex')).toBe(false);
+});
+
+test('isHexColor returns false for invalid hex colors - Test 2', () => {
+  expect(functions.isHexColor('?89723')).toBe(false);
+});
